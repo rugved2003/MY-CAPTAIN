976a0587eadaab129908d419c3c725c9c986f166
rugved2003 <81752288+rugved2003@users.noreply.github.com>
2021-04-01 17:11:22 +0530

    c++ program to find the product of two numbers

---
+++ my captain 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{    
+    double x, y, product;
+    cout << "enter two numbers : " ;
+    cin >> x;
+    cin>>y;
+    product = x*y;
+    cout<<"product :"<<product<<endl;
+    return 0;
+}
